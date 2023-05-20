a0e4a3976434702ea520be4b36bcb680a9a2bbc8
ezwe22 <134137972+ezwe22@users.noreply.github.com>
2023-05-20 18:17:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 
-# Load the Beauty Classification model
-beauty_model = tf.keras.models.load_model('beauty_model.h5')
+# Load the Traffic Sign Classification model
+traffic_model = tf.keras.models.load_model('traffic_model.h5')
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('beauty_model.h5')
+    model = tf.keras.models.load_model('traffic_model.h5')
     return model
 
 def preprocess_image(image):
-    image = image.resize((224, 224))  # Resize image to match input size
+    image = image.resize((32, 32))  # Resize image to match input size
     image = np.array(image)  # Convert PIL Image to NumPy array
     image = image / 255.0  # Normalize pixel values between 0 and 1
     image = np.expand_dims(image, axis=0)  # Add batch dimension
@@ -21,15 +21,15 @@ def preprocess_image(image):
 def import_and_predict(image_data, model):
     image = preprocess_image(image_data)
     prediction = model.predict(image)
-    if prediction < 0.5:
-        class_label = "Average"
-    else:
-        class_label = "Beautiful"
+    class_index = np.argmax(prediction)
+    class_label = class_names[class_index]
     return class_label
 
 model = load_model()
+class_names = ['Stop', 'Yield', 'Speed Limit 30', 'Speed Limit 50', 'Speed Limit 60',
+               'Speed Limit 70', 'Speed Limit 80', 'No Overtaking', 'No Entry', 'Road Work']
 
-st.write("# Beauty Classification: Beautiful or Average")
+st.write("# Traffic Sign Classification")
 file = st.file_uploader("Select an image", type=["jpg", "png"])
 
 if file is None:
