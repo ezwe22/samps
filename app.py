768d2cdacff5479e283e4828522a9ef1ac67cd90
ezwe22 <134137972+ezwe22@users.noreply.github.com>
2023-05-20 18:25:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ from PIL import Image
 import numpy as np
 
 # Load the Traffic Sign Classification model
-traffic_model = tf.keras.models.load_model('my_model.h5')
+model = tf.keras.models.load_model('my_model.h5')
 
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model = tf.keras.models.load_model('my_model.h5')
-    return model
+class_names = ['Stop', 'Yield', 'Speed Limit 30', 'Speed Limit 50', 'Speed Limit 60',
+               'Speed Limit 70', 'Speed Limit 80', 'No Overtaking', 'No Entry', 'Road Work']
 
 def preprocess_image(image):
     image = image.resize((32, 32))  # Resize image to match input size
@@ -24,10 +22,6 @@ def import_and_predict(image_data, model):
     class_index = np.argmax(prediction)
     class_label = class_names[class_index]
     return class_label
-
-model = load_model()
-class_names = ['Stop', 'Yield', 'Speed Limit 30', 'Speed Limit 50', 'Speed Limit 60',
-               'Speed Limit 70', 'Speed Limit 80', 'No Overtaking', 'No Entry', 'Road Work']
 
 st.write("# Traffic Sign Classification")
 file = st.file_uploader("Select an image", type=["jpg", "png"])
